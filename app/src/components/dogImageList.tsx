71ee998053979d44aab715ee8de082a28be96d58
An Le <lethienan3196@gmail.com>
2024-10-10 02:25:53 +0700

    load more

--- app/src/components/dogImageList.tsx
+++ app/src/components/dogImageList.tsx
@@ -1,26 +1,43 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import DogImage from './dogImage';
 import getDogs from '../api/dogImages';
 import styles from '../styles/dogList.module.css'
 
 export default function DogImageList() {
-    const [images, setImages] = useState([])
+    const [images, setImages] = useState([]);
+    const [dogTotal, setDogTotal] = useState(20);
+    const getData = async () => {
+        const data = await getDogs(dogTotal)
+        setImages((prev) => [...prev, ...data.message])
+    }
+
+    const handleScroll = () => {
+        const offsetHeight = document.documentElement.offsetHeight;
+        const innerHeight = window.innerHeight;
+        const scrollTop = document.documentElement.scrollTop;
+        const hasReachedBottom = offsetHeight - (innerHeight + scrollTop) <= 200;
+        hasReachedBottom && loadMoreOnClick()
+    };
 
     useEffect(() => {
-        const getData = async () => {
-            const data = await getDogs(20)
-            setImages(data.message)
-        }
         getData()
-    }, [])
+        window.addEventListener("scroll", handleScroll);
 
-    const listDogs = images.map((image) =>
-        <div className={styles.imageWrap} key={Math.floor(Math.random() * 100)}><DogImage src={image} /></div>
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [dogTotal])
+
+    const loadMoreOnClick = () => {
+        if (dogTotal > 200) return
+        setDogTotal((prev) => prev + 20);
+    }
+
+    const listDogs = images.map((image, index) =>
+        <div className={styles.imageWrap} key={index}><DogImage src={image} /></div>
     );
 
-    return <div className={styles.imageList}>
+    return <div className={styles.imageList} onScroll={handleScroll}>
         {listDogs}
     </div>
 }
